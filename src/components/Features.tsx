96a6af294da3f4c9dd4b437efc72b710caee049c
dyyllaann <dylan.arveson@gmail.com>
2023-04-26 15:31:44 -0700

    Improve Features section by using React methodology. Improve alignment in Features section.

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -6,7 +6,7 @@ import LazyShow from './LazyShow';
 
 const Features = () => {
   const { features } = config;
-  const [firstItem, secondItem, thirdItem] = features.items;
+  const featureItems = features.items;
 
   return (
     <section className={`bg-background pt-8`} id="product">
@@ -17,67 +17,31 @@ const Features = () => {
           {features.title}
         </h1>
         <Divider />
-        <LazyShow>
-          <div className={`flex flex-wrap`}>
-            <div className={`w-5/6 sm:w-1/2 p-6 mt-20`}>
-              <h3
-                className={`text-3xl text-gray-800 font-bold leading-none mb-3`}
-              >
-                {firstItem?.title}
-              </h3>
-              <p className={`text-gray-600`}>{firstItem?.description}</p>
-            </div>
-            <div className={`w-full sm:w-1/2 p-6`}>
-              <img
-                className="h-5/6 shadow-md"
-                src={firstItem?.img}
-                alt={firstItem?.title}
-              />
-            </div>
-          </div>
-        </LazyShow>
-        <LazyShow>
-          <div className={`flex flex-wrap flex-col-reverse sm:flex-row`}>
-            <div className={`w-full sm:w-1/2 p-6`}>
-              <img
-                className="h-5/6 shadow-md"
-                src={secondItem?.img}
-                alt={secondItem?.title}
-              />
-            </div>
-            <div className={`w-full sm:w-1/2 p-6 mt-20`}>
-              <div className={`align-middle`}>
-                <h3
-                  className={`text-3xl text-gray-800 font-bold leading-none mb-3`}
-                >
-                  {secondItem?.title}
-                </h3>
-                <p className={`text-gray-600 mb-8`}>
-                  {secondItem?.description}
-                </p>
+        {featureItems.map((item, index) => {
+          return (
+            <LazyShow key={index}>
+              <div className={`flex flex-wrap flex-col-reverse sm:flex-row`}>
+                <div className={`w-full sm:w-1/2 p-6`}>
+                  <img
+                    className="h-5/6 shadow-md mx-auto"
+                    src={item.img}
+                    alt={item.title}
+                  />
+                </div>
+                <div className={`w-full sm:w-1/2 p-6 flex`}>
+                  <div className={`self-center`}>
+                    <h3
+                      className={`text-3xl text-gray-800 font-bold leading-none mb-3`}
+                    >
+                      {item.title}
+                    </h3>
+                    <p className={`text-gray-600 mb-8`}>{item.description}</p>
+                  </div>
+                </div>
               </div>
-            </div>
-          </div>
-        </LazyShow>
-        <LazyShow>
-          <div className={`flex flex-wrap`}>
-            <div className={`w-5/6 sm:w-1/2 p-6 mt-20`}>
-              <h3
-                className={`text-3xl text-gray-800 font-bold leading-none mb-3`}
-              >
-                {thirdItem?.title}
-              </h3>
-              <p className={`text-gray-600`}>{thirdItem?.description}</p>
-            </div>
-            <div className={`w-full sm:w-1/2 p-6`}>
-              <img
-                className="h-5/6 shadow-md"
-                src={thirdItem?.img}
-                alt={thirdItem?.title}
-              />
-            </div>
-          </div>
-        </LazyShow>
+            </LazyShow>
+          );
+        })}
       </div>
     </section>
   );
